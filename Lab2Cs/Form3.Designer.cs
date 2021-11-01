2d96841347215e4ef081607f6ff8199a3f61e97d
Евгений Афанасьев <evgenij29082002@gmail.com>
2021-11-01 16:57:20 +0800

    подредачил 3 форму

--- Lab2Cs/Form3.Designer.cs
+++ Lab2Cs/Form3.Designer.cs
@@ -29,10 +29,20 @@ namespace Lab2Cs
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.SuspendLayout();
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form3";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form3";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
